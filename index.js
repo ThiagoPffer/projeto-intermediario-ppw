f4cac8b9b208ffd718f0fa0175044adf4760fbd7
Thiago Piffer <thiago.piffer@hotmail.com>
2021-05-27 18:38:43 -0300

    Retorno da tabela e conversao em array, adicao de dependencias

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
 const express = require('express');
+const axios = require('axios');
+const parser = require('node-html-parser').parse;
 const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.get('/', function(req, res) {
-    const cursos = [];
+    const url = "https://pt.wikipedia.org/wiki/Unidades_federativas_do_Brasil";
+    const estados = [];
 
-    res.json(cursos);
+    var requisicao = axios.get(url);
+
+    // var estado {
+    //     "nome":
+    //     "capital":
+    //     "IDH":
+    // }
+
+    requisicao.then(function(resposta) {
+        var root = parser(resposta.data);
+        var table = root.querySelector(".wikitable");
+        var tableRows = table.querySelectorAll("tr");
+        var content = [];
+        var filteredContent = [];
+
+        for (let i = 1; i < tableRows.length; i++) {
+            content[i] = tableRows[i].textContent.split("\n");
+        }
+
+        for (let i = 1; i < content.length; i++) {
+            for (let j = 0; j < content[i].length; j++) {
+                filteredContent[i] = content[i].filter(function(value) {
+                    return value != '';
+                });
+            }
+        }
+
+        console.log(filteredContent[24]);
+    });
+
+    res.json(estados);
 });
 
 app.listen(PORT, function() {
